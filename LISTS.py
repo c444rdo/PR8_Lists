a00b39ff6204d248e525c108a6c6df992b37b7e9
Ricardo Martínez <a.23308051281193@cbtis128.edu.mx>
2024-09-25 20:39:28 -0600

    Add files via upload

---
+++ LISTS.py
@@ -0,0 +1,68 @@
+# Código creado por la mera vena (Ricardo)
+"""""
+print ("Martínez Estrada Ricardo")
+print (" ")
+
+# Las listas son para poner diferentes valores a una sola variable.
+# Las listas se crean usando corchetes.
+# Las listas están indexadas. Es decir, el primer valor tendrá la posición "0", el segundo valor tendrá la posición "1", y así sucesivamente.
+# La forma para hacer una lista es la siguiente.
+
+listaejemplo = ["manzana", "platano", "cereza"]
+print(listaejemplo)
+print (" ")
+
+# Utilizando la función (len(x)) podemos ver la cantidad de "mini variables" que contiene una variable.
+listaejemplo2 = ["durazno", "kiwi", "naranja"]
+print(len(listaejemplo2))
+print (" ")
+
+# Dentro de las listas podemos poner caracteres, valores enteros o booleanos.
+lista1 = ["manzana", "platano", "cereza"]
+lista2 = [1, 5, 7, 9, 3]
+lista3 = [True, False, False]
+print (lista1)
+print (lista2)
+print (lista3)
+print (" ")
+
+# Podemos mezclar el contenido de las listas.
+listaejemplo3 = ["abc", 34, True, 40, "male"]
+print (listaejemplo3)
+"""""
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+# Código mandado por el profesor
+print ("Practica 8  donde usamos listas")
+print (" ")
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# Cuando usamos (len( )) te despliega el numero de elementos de la lista
+thislist2 = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# Separa con un espacio
+print(" ")
+
+# Da valores a varias listas
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+list4 = ["abc", 34, True, 40, "male"]
+
+# Muestra los miembros de cada lista
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(" ")
+thislist = ["apple", "banana", "cherry"]
+
+# Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1])
+
+# Muestra el ultimo miembro de la lisa
+print(thislist[-1])
+print(" ")
